bf926256df10cb554a1ce15d70db9d31560d9fd2
Jyoti Kumari <jkumari@incomm.com>
2019-05-23 15:45:33 -0400

    adding the new user creation functinality

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,6 +4,8 @@ function sideBar_Open() {
   document.getElementById("mySidebar").style.width = "25%";
   document.getElementById("mySidebar").style.display = "block";
   document.getElementById("openNav").style.display = 'none';
+  document.getElementById("curr_list").style.visibility = 'hidden';
+  document.getElementById("logo").style.visibility = 'hidden';
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
